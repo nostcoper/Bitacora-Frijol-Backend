81b504b0c28a90bb57f2182c4185f9d83bfbf2f4
nostcoper <carlosserrato200475@gmail.com>
2025-10-26 21:28:48 -0500

    fix: force .env

--- bitacoraBackend/app/settings.py
+++ bitacoraBackend/app/settings.py
@@ -10,12 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import Config, RepositoryEnv
 from pathlib import Path
-from decouple import config
 from datetime import timedelta
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+config = Config(RepositoryEnv(BASE_DIR / '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
